cd80d41efb26c5080cd151a30a677d492fcaf79f
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-10-20 22:51:26 +0200

    25.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '375217375756-omohn85m5r089u10e0q49fsja6h3tcbb.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'SbM0ZbsiHnEiOOB1zixqqi_j',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
